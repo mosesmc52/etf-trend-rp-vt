1ed15ab8ba79ce54767a9a38067e5d32cf0e7899
Moses McCall <mosesmc52@gmail.com>
2026-01-08 20:41:15 -0500

    inserted email in trend monitoring which was previously removed

--- algo.py
+++ algo.py
@@ -48,3 +48,48 @@ portfolio = run_single_iteration(
     ignore_liquidation_symbols=LIQUIDATION_SYMBOLS_TO_IGNORE,
 )
 print_orders_table(portfolio)
+
+
+# # Email Positions
+EMAIL_POSITIONS = str2bool(os.getenv("EMAIL_POSITIONS", False))
+#
+message_body_html = f"Portfolio Value: {portfolio_value}<br>"
+message_body_plain = f"Portfolio Value: {portfolio_value}\n"
+for position in portfolio["orders"]:
+    reason = position.get("reason", "")
+    suffix = f" [{reason}]" if reason else ""
+
+    message_body_html += (
+        f'<a clicktracking=off href="https://finviz.com/quote.ashx?t={position["symbol"]}">'
+        f'{position["symbol"]}</a>: {position["target_qty"]} '
+        f'({position["action"]} {round(position["alloc_w"],3)}){suffix}<br>'
+    )
+
+    message_body_plain += (
+        f'{position["symbol"]}: {position["target_qty"]} '
+        f'({position["action"]} {round(position["alloc_w"], 3)}){suffix}\n'
+    )
+
+if EMAIL_POSITIONS:
+    TO_ADDRESSES = os.getenv("TO_ADDRESSES", "").split(",")
+    FROM_ADDRESS = os.getenv("FROM_ADDRESS", "")
+    ses = AmazonSES(
+        region=os.environ.get("AWS_SES_REGION_NAME"),
+        access_key=os.environ.get("AWS_SES_ACCESS_KEY_ID"),
+        secret_key=os.environ.get("AWS_SES_SECRET_ACCESS_KEY"),
+        from_address=os.environ.get("FROM_ADDRESS"),
+    )
+    if LIVE_TRADE:
+        status = "Live"
+    else:
+        status = "Test"
+
+    subject = "Montly Trend Algo Report - {}".format(status)
+
+    for to_address in TO_ADDRESSES:
+        ses.send_html_email(
+            to_address=to_address, subject=subject, content=message_body_html
+        )
+
+print("---------------------------------------------------\n")
+print(message_body_plain)
